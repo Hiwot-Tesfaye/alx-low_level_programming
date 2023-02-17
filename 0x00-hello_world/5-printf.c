e553e2002c324d625497490d2eadc58d187893d0
Hiwot-Tesfaye <hiwottesfayegde@gmail.com>
2023-02-16 16:50:00 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints "with proper grammar, but the outcome is a piece of art," followed by new line
+ * Return: Always 0.
+ *
+ */
+
+int main(void)
+{
+
+printf("with proper grammar, but the outcome is a pieceof art,\n");
+return (0);
+
+
+
+}
