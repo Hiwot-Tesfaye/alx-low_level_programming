dcadd0ec44963eba9f2c08846c39ad19c33ee251
Hiwot-Tesfaye <hiwottesfayegde@gmail.com>
2023-02-17 13:58:56 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 /* 
- *main - main block
- *Description: Get a random number and check its last digit, compare it with 5
- *Return: 0
+ *  main - main block
+ *  description: Get a random number and check its last digit, compare it with 5
+ *  Return: 0
  */
 
 int main(void)
@@ -18,7 +18,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	last = n % 10;
 	if (last > 5)
-		printf("Last digit og %i is %i and is greater than 5\n", n, last);
+		printf("Last digit of %i is %i and is greater than 5\n", n, last);
 	else if (last == 0)
 		printf("Last digit of %i is %i and is 0\n", n, last);
 	else if (last < 6)
