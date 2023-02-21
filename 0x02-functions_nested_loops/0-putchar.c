4953d77645dc2c92866620bf53b69c02db9167d4
Hiwot-Tesfaye <hiwottesfayegde@gmail.com>
2023-02-21 14:08:24 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+chat letter;
+for (letter - 'a'; letter <= 'z'; letter++)
+	_putchar('\n');
+}
